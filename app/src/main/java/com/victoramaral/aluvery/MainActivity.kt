d2a2826dcd75fec128114d1d69c271979dda9864
Victor Amaral <vic-am@hotmail.com>
2023-10-24 23:09:37 -0300

    Adds Aluvery project

--- app/src/main/java/com/victoramaral/aluvery/MainActivity.kt
+++ app/src/main/java/com/victoramaral/aluvery/MainActivity.kt
@@ -1,37 +1,191 @@
 package com.victoramaral.aluvery
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.victoramaral.aluvery.extensions.toBrazilianCurrency
+import com.victoramaral.aluvery.model.Product
 import com.victoramaral.aluvery.ui.theme.AluveryTheme
+import com.victoramaral.aluvery.ui.theme.Purple500
+import com.victoramaral.aluvery.ui.theme.Teal200
+import java.math.BigDecimal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AluveryTheme {
-                Surface {
+            App()
+        }
+    }
+}
 
-                }
+@Composable
+private fun App() {
+    AluveryTheme {
+        Surface {
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .verticalScroll(
+                        rememberScrollState()
+                    ),
+verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Spacer(Modifier)
+                ProductSection()
+                ProductSection()
+                ProductSection()
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun AppPreview() {
+    App()
+}
+
+@Composable
+fun ProductSection() {
+    Column {
+        Text(
+            text = "Promoções",
+            Modifier
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp
+                ),
+            fontSize = 20.sp,
+            fontWeight = FontWeight(400)
+        )
+        Row(
+            Modifier
+                .padding(
+                    top = 8.dp
+                )
+                .fillMaxWidth()
+                .horizontalScroll(
+                    rememberScrollState()
+                ),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        )
+        {
+            Spacer(Modifier)
+            ProductItem(
+                Product(
+                    name = "Hambúrguer",
+                    price = BigDecimal("18.90"),
+                    image = R.drawable.burger
+                )
+            )
+            ProductItem(
+                Product(
+                    name = "Fritas",
+                    price = BigDecimal("12.90"),
+                    image = R.drawable.fries
+                )
+            )
+            ProductItem(
+                Product(
+                    name = "Pizza",
+                    price = BigDecimal("14.90"),
+                    image = R.drawable.pizza
+                )
+            )
+            Spacer(Modifier)
+        }
+    }
+}
+
+@Composable
+fun ProductItem(product: Product) {
+    Surface(
+        shape = RoundedCornerShape(15.dp),
+        shadowElevation = 4.dp
+    ) {
+        Column(
+            Modifier
+                .heightIn(250.dp, 300.dp)
+                .width(200.dp)
+        ) {
+            val imageSize = 100.dp
+            Box(
+                modifier = Modifier
+                    .height(imageSize)
+                    .fillMaxWidth()
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Purple500,
+                                Teal200
+                            )
+                        )
+                    )
+            ) {
+                Image(
+                    painter = painterResource(
+                        id = product.image
+                    ),
+                    contentDescription = "Imagem do produto",
+                    Modifier
+                        .offset(y = imageSize / 2)
+                        .size(imageSize)
+                        .clip(shape = CircleShape)
+                        .align(Alignment.BottomCenter),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Spacer(modifier = Modifier.height(imageSize / 2))
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = product.name,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = product.price.toBrazilianCurrency(),
+                    Modifier.padding(top = 8.dp),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400)
+                )
             }
         }
     }
@@ -39,19 +193,18 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun ProductItem() {
-    Column() {
-        Box(
-            modifier = Modifier
-                .height(50.dp)
-                .width(50.dp)
-                .background(Color.Blue)
+private fun ProductSectionPreview() {
+    ProductSection()
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProductItemPreview() {
+    ProductItem(
+        Product(
+            name = LoremIpsum(50).values.first(),
+            price = BigDecimal("14.99"),
+            image = R.drawable.ic_launcher_background
         )
-        Image(painter = painterResource(
-            id = R.drawable.ic_launcher_background),
-            contentDescription = "Imagem do produto"
-        )
-        Text(text = "Texto 1")
-        Text(text = "Texto 1")
-    }
+    )
 }
